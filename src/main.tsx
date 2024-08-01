4cfd602655f8f8b6fe4e0c95d104bf5380f9541f
Wilder Ap <wilderasprilla321@gmail.com>
2024-08-01 11:03:34 -0500

    feature routes guard

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
+import App from '@/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
-import { router } from '@/router/router.tsx'
-import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,25 +1,51 @@
-import App from '@/App'
-import { HomePage } from '@/pages'
-import { createBrowserRouter } from 'react-router-dom'
+import type { Routes } from '@/types'
+import type { RouteObject } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
+import { fallbackRoutes } from './fallback'
+import { ProtectedRoute } from './guard/ProtectedRoute'
+import { privateRoutes } from './private'
+import { publicRoutes } from './public'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      /// Dashboard Routes
-      {
-        path: 'dashboard',
-        element: <HomePage />,
-        children: [],
-      },
+export const AppRouter = () => {
+  /** 
+    Generates route configurations based on input routes, 
+    optionally wrapping private routes with ProtectedRoute.
+  **/
+  const generateRouteConfig = (
+    routes: Routes[],
+    isPrivate = false
+  ): RouteObject[] => {
+    return routes.map((route) => {
+      // protected routes
+      const routeObject: RouteObject = {
+        path: route.path,
+        element: isPrivate ? (
+          <ProtectedRoute>{route.element}</ProtectedRoute>
+        ) : (
+          route.element
+        ),
+      }
 
-      /// Auth Routes
-      {
-        path: 'auth',
-        element: <App />,
-        children: [],
-      },
-    ],
-  },
-])
+      // Recursively handle & generate children routes
+      if (route.children) {
+        routeObject.children = generateRouteConfig(route.children)
+      }
+
+      return routeObject
+    })
+  }
+
+  const publicRouteObjects = generateRouteConfig(publicRoutes)
+  const privateRouteObjects = generateRouteConfig(privateRoutes, false)
+  const fallbackRouteObjects = generateRouteConfig(fallbackRoutes)
+
+  const routes = [
+    ...publicRouteObjects,
+    ...privateRouteObjects,
+    ...fallbackRouteObjects,
+  ]
+
+  const allRoutes = useRoutes(routes)
+
+  return <> {allRoutes} </>
+}
