363915c86a04ca0d49af944c9ea39d7087d20312
francorinque <francoagustinrinquecorreo@gmail.com>
2024-08-21 18:30:22 -0600

    Feat(homePage,posts.store): setup fetch posts and toast, console.log in store when error

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,9 +1,56 @@
+import CustomToast from '@/components/common/CustomToast';
+import { usePostsStore } from '@/store';
+import { useEffect } from 'react';
+
 export const HomePage = () => {
+  const loading = usePostsStore((state) => state.loading);
+  const error = usePostsStore((state) => state.error);
+  const posts = usePostsStore((state) => state.posts);
+  const getPosts = usePostsStore((state) => state.getPosts);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  if (loading) {
+    return (
+      <p>
+        <strong>Loading...</strong>
+      </p>
+    );
+  }
+
   return (
-    <section className="min-h-screen">
+    <section className='min-h-screen'>
+      <CustomToast
+        data={posts}
+        error={error}
+        msgSuccess='Posts fetched successfully'
+        msgError='Error while fetching posts'
+      />
+      {error ? (
+        <p className='text-red-500'>{error}</p>
+      ) : (
+        posts?.length > 0 && (
+          <ul>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <div>
+                  <h4>Title: </h4>
+                  <p>{post.title}</p>
+                </div>
+                <div>
+                  <h4>Body: </h4>
+                  <p>{post.body}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )
+      )}
       <h1>Home</h1>
     </section>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
